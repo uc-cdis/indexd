e941d0a6679f84d4e76fd3051e523fb3f565a92b
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-10-15 12:51:21 -0500

    simplifies driver

--- indexd/index/driver.py
+++ indexd/index/driver.py
@@ -10,73 +10,43 @@ class IndexDriverABC(object):
     '''
     __metaclass__ = abc.ABCMeta
 
-    def add(self, data):
-        '''
-        Creates record for given data.
-        '''
-        record = str(uuid.uuid4())
-        
-        self[record] = data
-        
-        return record
-
-    def get(self, record):
-        '''
-        Returns data for record if stored by backend.
-        Raises KeyError otherwise.
-        '''
-        return self[record]
-
-    def update(self, record, data):
-        '''
-        Replaces record if stored by backend.
-        Raises KeyError otherwise.
-        '''
-        if not record in self:
-            raise KeyError('record does not exist')
-        
-        self[record] = data
-
-    def delete(self, record):
-        '''
-        Deletes record if stored by backend.
-        Raises KeyError otherwise.
-        '''
-        del self[record]
-
     @abc.abstractmethod
-    def ids(self, limit=100, start='', size=None, hashes={}):
+    def ids(self, limit=100, start='', size=None, urls=None, hashes=None):
         '''
         Returns a list of records stored by the backend.
         '''
         raise NotImplementedError('TODO')
 
     @abc.abstractmethod
-    def __getitem__(self, record):
+    def add(self, form, size, urls=[], hashes={}):
         '''
-        Returns record if stored by backend.
-        Raises KeyError otherwise.
-        '''
-        raise NotImplementedError('TODO')
-
-    @abc.abstractmethod
-    def __setitem__(self, record, data):
-        '''
-        Replaces record if stored by backend.
-        Raises KeyError otherwise.
+        Creates record for given data.
         '''
         raise NotImplementedError('TODO')
 
     @abc.abstractmethod
-    def __delitem__(self, record):
+    def get(self, did):
         '''
-        Removes record if stored by backend.
-        Raises KeyError otherwise.
+        Gets a record given the record id.
         '''
         raise NotImplementedError('TODO')
 
     @abc.abstractmethod
-    def __contains__(self, record):
+    def update(self, did, rev, size=None, urls=None, hashes=None):
+        '''
+        Updates record with new values.
+        '''
+        raise NotImplementedError('TODO')
+
+    @abc.abstractmethod
+    def delete(self, did, rev):
+        '''
+        Deletes record.
+        '''
+        raise NotImplementedError('TODO')
+
+    @abc.abstractmethod
+    def __contains__(self, did):
         '''
         Returns True if record is stored by backend.
         Returns False otherwise.
