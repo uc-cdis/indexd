958edd42745a6772491ff9e04dd25a133ad189c4
philloooo <phillis.tt@gmail.com>
2017-01-31 13:29:53 -0600

    feat(admin): add script to create and delete user

---
+++ bin/index_admin.py
@@ -0,0 +1,41 @@
+import argparse
+import sys
+
+
+def main(path, action=None, username=None, password=None):
+    sys.path.append(path)
+    from local_settings import settings
+    driver = settings['auth']
+    if action == 'create':
+        try:
+            driver.add(username, password)
+            print 'User {} created'.format(username)
+        except Exception as e:
+            print e.message
+
+    elif action == 'delete':
+        try:
+            driver.delete(username)
+            print 'User {} deleted'.format(username)
+        except Exception as e:
+            print e.message
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--path',
+        default='/var/www/',
+        help='path to find local_settings.py',
+    )
+    subparsers = parser.add_subparsers(title='action', dest='action')
+    create = subparsers.add_parser('create')
+    delete = subparsers.add_parser('delete')
+    create.add_argument('--username', required=True)
+    create.add_argument('--password', required=True)
+    delete.add_argument('--username', required=True)
+    args = parser.parse_args()
+
+    main(**args.__dict__)
