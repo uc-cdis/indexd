38d90974a30a8e1930032a4ec1585d0e6a33041f
ProfOak <OpenProfOak@gmail.com>
2019-03-08 14:04:05 -0600

    feat(jsonb): add index tables migration script
    The migration script just instantiates the index driver with
    auto_migrate=True. The init function of the driver runs through
    all the migrations.

---
+++ bin/migrate_index.py
@@ -0,0 +1,19 @@
+"""
+The index sqlalchemy driver __init__ function runs all the necessary migration
+functions. This will run every migration from the current version as noted in
+the database to the latest migration function in the list.
+"""
+import os
+
+from indexd.index.drivers.alchemy import SQLAlchemyIndexDriver
+
+if __name__ == '__main__':
+    SQLAlchemyIndexDriver(
+        'postgresql://{username}:{password}@{hostname}:{port}/{database}'.format(
+            username=os.environ.get('PG_USER'),
+            password=os.environ.get('PG_PASS'),
+            hostname=os.environ.get('PG_HOST'),
+            port=os.environ.get('PG_PORT'),
+            database=os.environ.get('PG_DATABASE'),
+        )
+    )
