993f30515379553c0ef06cfc602e6c0d4e4dbd45
vpsx <19900057+vpsx@users.noreply.github.com>
2019-05-24 13:11:25 -0500

    bump sqlalchemy and utils in setup (#217)
    * bump sqlalchemy and utils in setup
    
    * python 3.4-->3.6 in travis

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     install_requires=[
         'flask==0.12.4',
         'jsonschema==2.5.1',
-        'sqlalchemy==1.0.8',
-        'sqlalchemy-utils>=0.32.21',
+        'sqlalchemy==1.3.3',
+        'sqlalchemy-utils>=0.33.11',
         'psycopg2>=2.7',
         'future>=0.16.0,<1.0.0',
         'cdislogging',
