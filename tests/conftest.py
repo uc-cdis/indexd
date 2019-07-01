faea8af145f58721ff9a6a21dbd4efcef518ddbc
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-07-01 15:40:13 -0500

    chore(python3): remove py2 support

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,12 +11,9 @@ from indexd import auth
 import importlib
 
 try:
-    reload  # Python 2.7
-except NameError:
-    try:
-        from importlib import reload  # Python 3.4+
-    except ImportError:
-        from imp import reload  # Python 3.0 - 3.3<Paste>
+    from importlib import reload  # Python 3.4+
+except ImportError:
+    from imp import reload  # Python 3.0 - 3.3<Paste>
 
 
 @pytest.fixture
