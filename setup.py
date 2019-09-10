df23db1d585a040e1f335a081d4fd03cddf4d7d4
Maksim An <anmaxvl@gmail.com>
2019-09-10 10:43:09 -0500

    bump deps

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'flask==0.12.4',
         'jsonschema==2.5.1',
-        'sqlalchemy==1.0.8',
+        'sqlalchemy',
         'sqlalchemy-utils>=0.32.21',
         'psycopg2>=2.7',
         'future>=0.16.0,<1.0.0',
@@ -23,7 +23,7 @@ setup(
     ],
     dependency_links=[
         "git+https://github.com/uc-cdis/cdislogging.git@0.0.2#egg=cdislogging",
-        "git+https://github.com/uc-cdis/indexclient.git@1.3.1#egg=indexclient",
+        "git+https://github.com/NCI-GDC/indexclient.git@1.3.10#egg=indexclient",
         "git+https://github.com/uc-cdis/doiclient.git@1.0.0#egg=doiclient",
         "git+https://github.com/uc-cdis/dosclient.git@1.0.0#egg=dosclient",
     ],
