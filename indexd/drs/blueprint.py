a22f8b4a3e9e32a0f8308539d416d5b0ac39ef77
giangbui <giangbui0816@gmail.com>
2021-01-11 19:08:42 -0600

    fixup

--- indexd/drs/blueprint.py
+++ indexd/drs/blueprint.py
@@ -266,7 +266,7 @@ def parse_checksums(record, drs_object):
         except json.decoder.JSONDecodeError:
             # TODO: is it expected that the record["checksum"] is json format?
             # it seems that it is string
-            checksums = [record["checksum"]]
+            checksums = [{"checksum": record["checksum"], "type": "md5"}]
         for checksum in checksums:
             ret_checksum.append(
                 {"checksum": checksum["checksum"], "type": checksum["type"]}
