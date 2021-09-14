955fc409be6e50224ee6f2e130d98351b7a447da
MaribelleHGomez <maribellehgomez@gmail.com>
2021-09-14 11:29:06 -0500

    default pre

--- indexd/drs/blueprint.py
+++ indexd/drs/blueprint.py
@@ -101,7 +101,7 @@ def indexd_to_drs(record, expand=False):
         else record["bundle_id"]
     )
 
-    print(blueprint.index_driver)
+    print(blueprint.index_driver.get("DEFAULT_PREFIX"))
     defaultPrefix = "test"
     # defaultPrefix = blueprint.index_driver.get("DEFAULT_PREFIX")
     accession = did
