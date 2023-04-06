73e0a7c69e5c29cea7da10c1662ed4b2caffba6f
Rowland Ogwara <rogwara@uchicago.edu>
2023-04-06 17:28:33 -0500

    re-order checks

--- docker.sh
+++ docker.sh
@@ -8,16 +8,15 @@ DEFAULT_REGISTRY="${DOCKER_RELEASE_REGISTRY:=quay.io/ncigdc}"
 # avoid installing git
 COMMIT=$(git rev-parse HEAD) && echo "COMMIT=\"${COMMIT}\"" >indexd/index/version_data.py
 VERSION=$(git describe --always --tags) && echo "VERSION=\"${VERSION}\"" >>indexd/index/version_data.py
-GIT_BRANCH=${TRAVIS_BRANCH:~CI_COMMIT_BRANCH}
 IMAGE_NAME=${DEFAULT_REGISTRY}/indexd
 
 # setup active branch name, default to using git if build is happening on local
-if [ -z ${GIT_BRANCH+x} ]; then
+if [ -z ${TRAVIS_BRANCH+x} ]; then
   GIT_BRANCH=$(git symbolic-ref --short -q HEAD);
 elif [ -z ${GIT_BRANCH+x} ]; then
-  GIT_BRANCH=$TRAVIS_BRANCH;
-else
   GIT_BRANCH=$CI_COMMIT_BRANCH;
+else
+  GIT_BRANCH=$TRAVIS_BRANCH;
 fi
 
 # replace slashes with underscore
