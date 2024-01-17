3432ed9065034d28a5aeda6a06789e67d479e2cb
Qiao Qiao <68757394+qiaouchicago@users.noreply.github.com>
2024-01-17 10:14:55 -0600

    DEV 2406 fix auto deploy (#99)
    * update docker.sh
    
    * update Dockerfile
    
    * update gitlab-ci template version
    
    * fix docker.sh for travis

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,39 @@
 ARG base_version=1.2.0
 ARG registry=quay.io
+ARG NAME=indexd
 
 FROM ${registry}/ncigdc/python38-builder:${base_version} as build
+ARG NAME
+ARG PIP_INDEX_URL
+ENV PIP_INDEX_URL=$PIP_INDEX_URL
+ARG REQUIREMENTS_GDC_LIBRARIES_FILE
 
+WORKDIR /${NAME}
 # Copy only requirements.txt here so Docker can cache the layer with
 # the installed packages if the pins don't change.
-COPY requirements.txt /indexd/
-WORKDIR /indexd
+COPY requirements.txt ./
 RUN pip3 install --no-deps -r requirements.txt
 
 # Now install the code for indexd itself.
-COPY . /indexd
+COPY . .
 RUN pip3 install --no-deps .
 
 
 FROM ${registry}/ncigdc/python38-httpd:${base_version}
+ARG NAME
 
-LABEL org.opencontainers.image.title="indexd" \
-      org.opencontainers.image.description="indexd container image" \
-      org.opencontainers.image.source="https://github.com/NCI-GDC/indexd" \
+LABEL org.opencontainers.image.title=${NAME} \
+      org.opencontainers.image.description="${NAME} container image" \
+      org.opencontainers.image.source="https://github.com/NCI-GDC/${NAME}" \
       org.opencontainers.image.vendor="NCI GDC"
 
 
-RUN mkdir -p /var/www/indexd/ \
-  && chmod 777 /var/www/indexd \
+RUN mkdir -p /var/www/${NAME}/ \
+  && chmod 777 /var/www/${NAME} \
   && a2dissite 000-default
 
-COPY wsgi.py /var/www/indexd/
-COPY bin/indexd /var/www/indexd/
+COPY wsgi.py /var/www/${NAME}/
+COPY bin/indexd /var/www/${NAME}/
 COPY --from=build /usr/local/lib/python3.8/dist-packages /usr/local/lib/python3.8/dist-packages
 
 # Make indexd CLI utilities available for, e.g., DB schema migration.
@@ -37,4 +43,4 @@ RUN ln -sf /dev/stdout /var/log/apache2/access.log \
   && ln -sf /dev/stdout /var/log/apache2/other_vhosts_access.log\
   && ln -sf /dev/stderr /var/log/apache2/error.log
 
-WORKDIR /var/www/indexd
+WORKDIR /var/www/${NAME}
--- docker.sh
+++ docker.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-set -eo pipefail
+set -euox pipefail
 
-PARAM=${1:-push};
+NAME="indexd"
+PARAM=${1:-DO_NOT_PUSH};
 
-IMAGE_NAME="${DOCKER_RELEASE_REGISTRY:=quay.io}/ncigdc/indexd"
+IMAGE_NAME="${DOCKER_RELEASE_REGISTRY:=quay.io}/ncigdc/${NAME}"
+REGISTRY="${BASE_CONTAINER_REGISTRY:=quay.io}"
 
-# avoid installing git
-COMMIT=$(git rev-parse HEAD) && echo "COMMIT=\"${COMMIT}\"" >indexd/index/version_data.py
+if [ -f 'VERSION.txt' ]; then
+  VERSION=$(cat VERSION.txt)
+else
+  VERSION=$(python -m setuptools_scm)
+fi
 
 # setup active branch name, default to using git if build is happening on local
 if [ ${TRAVIS_BRANCH+x} ]; then
   GIT_BRANCH=$TRAVIS_BRANCH;
 elif [ ${GITLAB_CI+x} ]; then
-  GIT_BRANCH=${CI_COMMIT_REF_SLUG}
+  GIT_BRANCH=${CI_COMMIT_REF_NAME};
 else
   GIT_BRANCH=$(git symbolic-ref --short -q HEAD);
 fi
@@ -20,20 +25,24 @@ fi
 # replace slashes with underscore
 GIT_BRANCH=${GIT_BRANCH/\//_}
 
-VERSION=$(cat VERSION.txt)
-echo "$VERSION"
+# Save the commit hash so the /status endpoint doesn't need Git.
+COMMIT=$(git rev-parse HEAD)
+echo "COMMIT=\"${COMMIT}\"" >indexd/index/version_data.py
 
 BUILD_COMMAND=(build \
   --label org.opencontainers.image.version="${VERSION}" \
   --label org.opencontainers.image.created="$(date -Iseconds)" \
-  --label org.opencontainers.image.revision="$(git rev-parse HEAD)" \
-  --label org.opencontainers.ref.name="indexd:${GIT_BRANCH}" \
-  --ssh default -t "$IMAGE_NAME:$GIT_BRANCH")
-
-echo "${BUILD_COMMAND[@]}"
+  --label org.opencontainers.image.revision="${COMMIT}" \
+  --label org.opencontainers.ref.name="${NAME}:${GIT_BRANCH}" \
+  --build-arg REGISTRY="${REGISTRY%\/ncigdc}" \
+  --build-arg BASE_VERSION="${BASE_CONTAINER_VERSION:=1.2.0}" \
+  --build-arg PIP_INDEX_URL \
+  -t "$IMAGE_NAME:$GIT_BRANCH" \
+  -t "$IMAGE_NAME:$COMMIT")
 
 docker "${BUILD_COMMAND[@]}" .
 
 if [ "$PARAM" = "push" ]; then
-  docker push "$IMAGE_NAME:$GIT_BRANCH"
+  docker image ls "$IMAGE_NAME"
+  docker push -a "$IMAGE_NAME"
 fi
