eaf14c4260a0d9e7c8880f54c1c9ffd73657dbc4
BinamB <numb.baj@gmail.com>
2024-07-05 13:47:29 -0500

    undo select queries

--- bin/migrate_to_single_table.py
+++ bin/migrate_to_single_table.py
@@ -111,27 +111,16 @@ class IndexRecordMigrator:
                 if not records:
                     break
 
-                # Collect all dids in the current batch
-                dids = [record.did for record in records]
-                # Fetch related data for all dids in the current batch
-                hashes = self.get_index_record_hash(dids)
-                urls = self.get_urls_record(dids)
-                url_metadata = self.get_urls_metadata(dids)
-                acl = self.get_index_record_ace(dids)
-                authz = self.get_index_record_authz(dids)
-                alias = self.get_index_record_alias(dids)
-                metadata = self.get_index_record_metadata(dids)
-
                 records_to_insert = []
 
                 for record in records:
-                    record_hashes = hashes.get(record.did, {})
-                    record_urls = urls.get(record.did, [])
-                    record_url_metadata = url_metadata.get(record.did, {})
-                    record_acl = acl.get(record.did, [])
-                    record_authz = authz.get(record.did, [])
-                    record_alias = alias.get(record.did, [])
-                    record_metadata = metadata.get(record.did, {})
+                    hashes = self.get_index_record_hash(record.did)
+                    urls = self.get_urls_record(record.did)
+                    url_metadata = self.get_urls_metadata(record.did)
+                    acl = self.get_index_record_ace(record.did)
+                    authz = self.get_index_record_authz(record.did)
+                    alias = self.get_index_record_alias(record.did)
+                    metadata = self.get_index_record_metadata(record.did)
 
                     records_to_insert.append(
                         Record(
@@ -182,102 +171,78 @@ class IndexRecordMigrator:
             self.session.close()
             self.logger.info("Finished migrating :D")
 
-    def get_index_record_hash(self, dids):
+    def get_index_record_hash(self, did):
         try:
             stmt = (
                 self.session.query(
-                    IndexRecordHash.did,
                     IndexRecordHash.hash_type,
                     IndexRecordHash.hash_value,
                 )
-                .filter(IndexRecordHash.did.in_(dids))
+                .filter(IndexRecordHash.did == did)
                 .all()
             )
-            res = {}
-            for did, hash_type, hash_value in stmt:
-                if did not in res:
-                    res[did] = {}
-                res[did][hash_type] = hash_value
+            res = {hash_type: hash_value for hash_type, hash_value in stmt}
             return res
         except Exception as e:
-            self.logger.error(f"Error with hashes: {e}")
+            self.logger.error(f"Error with hash for {did}: {e}")
 
-    def get_urls_record(self, dids):
+    def get_urls_record(self, did):
         try:
             stmt = (
-                self.session.query(IndexRecordUrl.did, IndexRecordUrl.url)
-                .filter(IndexRecordUrl.did.in_(dids))
+                self.session.query(IndexRecordUrl.url)
+                .filter(IndexRecordUrl.did == did)
                 .all()
             )
-            res = {}
-            for did, url in stmt:
-                if did not in res:
-                    res[did] = []
-                res[did].append(url)
+            res = [u.url for u in stmt]
             return res
         except Exception as e:
-            self.logger.error(f"Error with urls: {e}")
+            self.logger.error(f"Error with urls for {did}: {e}")
 
-    def get_urls_metadata(self, dids):
+    def get_urls_metadata(self, did):
         try:
             stmt = (
                 self.session.query(
-                    IndexRecordUrlMetadata.did,
                     IndexRecordUrlMetadata.url,
                     IndexRecordUrlMetadata.key,
                     IndexRecordUrlMetadata.value,
                 )
-                .filter(IndexRecordUrlMetadata.did.in_(dids))
+                .filter(IndexRecordUrlMetadata.did == did)
                 .all()
             )
-            res = {}
-            for did, url, key, value in stmt:
-                if did not in res:
-                    res[did] = {}
-                if url not in res[did]:
-                    res[did][url] = {}
-                res[did][url][key] = value
+            res = {url: {key: value} for url, key, value in stmt}
             return res
         except Exception as e:
-            self.logger.error(f"Error with url metadata: {e}")
+            self.logger.error(f"Error with url metadata for {did}: {e}")
 
-    def get_index_record_ace(self, dids):
+    def get_index_record_ace(self, did):
         try:
             stmt = (
-                self.session.query(IndexRecordACE.did, IndexRecordACE.ace)
-                .filter(IndexRecordACE.did.in_(dids))
+                self.session.query(IndexRecordACE.ace)
+                .filter(IndexRecordACE.did == did)
                 .all()
             )
-            res = {}
-            for did, ace in stmt:
-                if did not in res:
-                    res[did] = []
-                res[did].append(ace)
+            res = [a.ace for a in stmt]
             return res
         except Exception as e:
-            self.logger.error(f"Error with ace: {e}")
+            self.logger.error(f"Error with ace for did {did}: {e}")
 
-    def get_index_record_authz(self, dids):
+    def get_index_record_authz(self, did):
         try:
             stmt = (
-                self.session.query(IndexRecordAuthz.did, IndexRecordAuthz.resource)
-                .filter(IndexRecordAuthz.did.in_(dids))
+                self.session.query(IndexRecordAuthz.resource)
+                .filter(IndexRecordAuthz.did == did)
                 .all()
             )
-            res = {}
-            for did, resource in stmt:
-                if did not in res:
-                    res[did] = []
-                res[did].append(resource)
+            res = [r.resource for r in stmt]
             return res
         except Exception as e:
             self.logger.error(f"Error with authz: {e}")
 
-    def get_index_record_alias(self, dids):
+    def get_index_record_alias(self, did):
         try:
             stmt = (
-                self.session.query(IndexRecordAlias.did, IndexRecordAlias.name)
-                .filter(IndexRecordAlias.did.in_(dids))
+                self.session.query(IndexRecordAlias.name)
+                .filter(IndexRecordAlias.did == did)
                 .all()
             )
             res = {}
@@ -289,25 +254,20 @@ class IndexRecordMigrator:
         except Exception as e:
             self.logger.error(f"Error with alias: {e}")
 
-    def get_index_record_metadata(self, dids):
+    def get_index_record_metadata(self, did):
         try:
             stmt = (
                 self.session.query(
-                    IndexRecordMetadata.did,
                     IndexRecordMetadata.key,
                     IndexRecordMetadata.value,
                 )
-                .filter(IndexRecordMetadata.did.in_(dids))
+                .filter(IndexRecordMetadata.did == did)
                 .all()
             )
-            res = {}
-            for did, key, value in stmt:
-                if did not in res:
-                    res[did] = {}
-                res[did][key] = value
+            res = [row.name for row in stmt]
             return res
         except Exception as e:
-            self.logger.error(f"Error with alias: {e}")
+            self.logger.error(f"Error with alias for did {did}: {e}")
 
     def remove_duplicate_records(self, records, error):
         # Extract the key value from the error message
