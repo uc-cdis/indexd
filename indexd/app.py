6a4a5e179d38e2970524e47c058f1c88b19d322c
Arthur Collet <acollet@uchicago.edu>
2024-11-26 14:43:13 -0600

    Testing ddtrace log injection

--- indexd/app.py
+++ indexd/app.py
@@ -15,7 +15,7 @@ from .index.blueprint import blueprint as indexd_index_blueprint
 
 def app_init(app, settings=None):
     app.logger.addHandler(cdislogging.get_stream_handler())
-    ddtrace.patch_all()
+    ddtrace.patch_all(logging=True)
     if not settings:
         from .default_settings import settings
     app.config.update(settings["config"])
