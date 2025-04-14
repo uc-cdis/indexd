43a82386c690549505eeb8f1a2c2362ee30781f9
BinamB <numb.baj@gmail.com>
2025-04-14 10:30:47 -0500

    add info

--- indexd/index/drivers/single_table_alchemy.py
+++ indexd/index/drivers/single_table_alchemy.py
@@ -116,7 +116,7 @@ class SingleTableSQLAlchemyIndexDriver(IndexDriverABC):
         self.config = index_config or {}
         Base.metadata.bind = self.engine
         self.Session = sessionmaker(bind=self.engine)
-        self.logger("------------SingleTable IndexD Driver--------------")
+        self.logger.info("------------SingleTable IndexD Driver--------------")
 
     @property
     @contextmanager
